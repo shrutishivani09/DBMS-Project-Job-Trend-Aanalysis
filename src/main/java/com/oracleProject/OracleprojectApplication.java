3dd8b67e9154fcdbcdcd1e2f94400ddc241bb2f3
shrutishivani09 <89985139+shrutishivani09@users.noreply.github.com>
2021-12-16 15:58:04 -0500

    Final

---
+++ src/main/java/com/oracleProject/OracleprojectApplication.java
@@ -0,0 +1,14 @@
+package com.oracleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleprojectApplication.class, args);
+	}
+
+}
+ 
